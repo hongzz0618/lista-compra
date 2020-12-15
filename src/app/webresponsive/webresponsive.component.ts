7895ad02be39ff2aea20bac622e0b305c3073b6c
Hong Zhou <hong@dmp.es>
2020-12-15 17:07:19 +0100

    responsive web notes

---
+++ src/app/webresponsive/webresponsive.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-webresponsive',
+  templateUrl: './webresponsive.component.html',
+  styleUrls: ['./webresponsive.component.scss'],
+})
+export class WebresponsiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
