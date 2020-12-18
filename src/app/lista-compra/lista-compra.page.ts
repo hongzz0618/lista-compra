169f8d99aec01409ecb6ac24ba470f0d93cfaa08
Hong Zhou <hong@dmp.es>
2020-12-19 00:54:24 +0100

    first angular learning commit

--- src/app/lista-compra/lista-compra.page.ts
+++ src/app/lista-compra/lista-compra.page.ts
@@ -10,10 +10,31 @@ import { ListaCompraService } from './lista-compra.service';
 export class ListaCompraPage implements OnInit {
 
   producto: Producto[];
-  constructor(private productoService: ListaCompraService) { }
+  page_name = "Lista de Compra"
+  text = ""
+  blank = ""
+  Xcli = "ngIf false"
+  button = false
+  log = [];
+  constructor(private productoService: ListaCompraService) {
 
-  ngOnInit() {
-    this.producto = this.productoService.getAllProducts();
   }
 
+  ngOnInit() {
+    // setTimeout(()=>{
+    //   this.button=false
+    // },2000)
+    this.producto = this.productoService.getAllProducts();
+  }
+  logmasuno() {
+    this.log.push(this.log.length + 1)
+  }
+  Xclick() {
+    this.Xcli = "ngIf true"
+    this.button = true
+    // this.text=this.blank
+  }
+  targetvalue(event: Event) {
+    this.text = (<HTMLInputElement>event.target).value
+  }
 }
