341f56a95dab1b50436edb73158620893b4d78a7
Hong Zhou <hong@dmp.es>
2020-12-26 13:06:27 +0100

    added two tabs menu

---
+++ src/app/pages/tab2/tab2.page.ts
@@ -0,0 +1,22 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page implements OnInit {
+
+
+  constructor() {
+
+  }
+
+
+  ngOnInit() {
+  }
+
+
+
+}
